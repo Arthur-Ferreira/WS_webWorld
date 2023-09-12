75a8299ae37f2de3e9b7eed855d3feb28ee8154f
Arthur-Ferreira <arthufdarosa@gmail.com>
2023-09-12 19:54:20 -0300

    Finishing Styles

--- scripts.js
+++ scripts.js
@@ -1,23 +1,11 @@
-const carousel = document.querySelector(".carousel-items");
-const partnes = document.querySelector(".partners-lst");
 const mobileMenuBtn = document.getElementById('mobile-menu');
 const mobileMenuNavigationElem = document.querySelector('.navigation');
 
-function scroll(event) {
-  if (event.deltaY > 0) {
-    event.target.scrollBy(300, 0);
-  } else {
-    event.target.scrollBy(-300, 0);
-  }
-}
-
-function toggleMenu(event) {
-  
+function toggleMenu(event) {  
   if(event.target.id === 'mobile-menu') {
     mobileMenuNavigationElem.classList.toggle('open');
   } 
 }
 
-addEventListener("wheel", scroll)
-addEventListener("wheel", scroll)
-addEventListener("click", toggleMenu)
+mobileMenuBtn.addEventListener("click", toggleMenu);
+
